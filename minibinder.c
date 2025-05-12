708d830687c865470299a301bbf03ac3724505ff
Boyao Hou <shangrila-1100@hotmail.com>
2025-05-12 20:17:24 +0800

    Update minibinder message format to include target PID and enhance write functionality

--- minibinder.c
+++ minibinder.c
@@ -107,7 +107,7 @@ static int minibinder_release(struct inode *inode, struct file *file) {
  * interrupted.
  *
  * The message format copied to user space is:
- *   [sender_pid][data_size][data]
+ *   [target_pid][sender_pid][data_size][data]
  *
  * On success, the message is removed from the queue and its memory is freed.
  *
@@ -144,24 +144,34 @@ static ssize_t minibinder_read(struct file *file, char __user *buf,
   }
 
   msg = list_first_entry(&entry->message_list, struct message, list);
-  msg_size = msg->data_size + sizeof(msg->sender_pid) + sizeof(msg->data_size);
+  msg_size = msg->data_size + sizeof(msg->sender_pid) +
+             sizeof(msg->target_pid) + sizeof(msg->data_size);
   if (count < msg_size) {
     pr_err("Buffer too small for message\n");
     ret = -EINVAL;
     goto out;
   }
 
-  if (copy_to_user(buf, &msg->sender_pid, sizeof(msg->sender_pid))) {
+  if (copy_to_user(buf, &msg->target_pid, sizeof(msg->target_pid))) {
+    pr_err("Failed to copy target PID to user space\n");
+    ret = -EFAULT;
+    goto out;
+  }
+
+  if (copy_to_user(buf + sizeof(msg->target_pid), &msg->sender_pid,
+                   sizeof(msg->sender_pid))) {
     pr_err("Failed to copy sender PID to user space\n");
     ret = -EFAULT;
     goto out;
   }
-  if (copy_to_user(buf + sizeof(msg->sender_pid), &msg->data_size,
-                   sizeof(msg->data_size))) {
+
+  if (copy_to_user(buf + sizeof(msg->sender_pid) + sizeof(msg->target_pid),
+                   &msg->data_size, sizeof(msg->data_size))) {
     pr_err("Failed to copy data size to user space\n");
     ret = -EFAULT;
     goto out;
   }
+
   if (copy_to_user(buf + sizeof(msg->sender_pid) + sizeof(msg->data_size),
                    msg->data, msg->data_size)) {
     pr_err("Failed to copy message data to user space\n");
@@ -180,8 +190,85 @@ out:
   return 0;
 }
 
+/**
+ * minibinder_write - Write a message to the minibinder device for a target
+ * process.
+ * @file:  Pointer to the file structure.
+ * @buf:   User-space buffer containing the message to write.
+ * @count: Size of the user-space buffer.
+ * @ppos:  File position pointer (unused).
+ *
+ * This function writes a message to the minibinder device, targeting a specific
+ * process identified by its PID. The message format in the user-space buffer
+ * is: [target_pid][sender_pid][data_size][data]
+ *
+ * Steps:
+ * 1. Extract the target PID and message size from the user-space buffer.
+ * 2. Allocate memory for the message and copy the message data from user space.
+ * 3. Retrieve or create a `target_pid_entry` for the target PID.
+ * 4. Add the message to the target process's message queue.
+ * 5. Wake up any process waiting on the target's message queue.
+ *
+ * Return:
+ *   - On success: The number of bytes written (equal to `count`).
+ *   - On failure: A negative error code, such as:
+ *       - `-EINVAL` if the buffer is too small.
+ *       - `-EFAULT` if copying from user space fails.
+ *       - `-ENOMEM` if memory allocation fails.
+ */
 static ssize_t minibinder_write(struct file *file, const char __user *buf,
                                 size_t count, loff_t *ppos) {
+  pid_t target_pid, sender_pid;
+  struct target_pid_entry *target_entry;
+  struct message *msg;
+  size_t msg_size;
+
+  if (count < sizeof(target_pid) + sizeof(sender_pid) + sizeof(size_t)) {
+    pr_err("Buffer too small for target PID, sender PID and data size\n");
+    return -EINVAL;
+  }
+  if (copy_from_user(&target_pid, buf, sizeof(target_pid))) {
+    pr_err("Failed to copy target PID from user space\n");
+    return -EFAULT;
+  }
+  if (copy_from_user(&sender_pid, buf + sizeof(target_pid),
+                     sizeof(sender_pid))) {
+    pr_err("Failed to copy sender PID from user space\n");
+    return -EFAULT;
+  }
+  if (copy_from_user(&msg_size, buf + sizeof(target_pid) + sizeof(sender_pid),
+                     sizeof(size_t))) {
+    pr_err("Failed to copy message size from user space\n");
+    return -EFAULT;
+  }
+
+  msg = kmalloc(sizeof(*msg), GFP_KERNEL);
+  if (!msg) {
+    pr_err("Failed to allocate memory for message\n");
+    return -ENOMEM;
+  }
+  if (copy_from_user(msg->data, buf + sizeof(target_pid) + sizeof(size_t),
+                     msg_size)) {
+    pr_err("Failed to copy message data from user space\n");
+    kfree(msg);
+    return -EFAULT;
+  }
+
+  target_entry = get_target_pid_entry(target_pid, true);
+  if (!target_entry) {
+    pr_err("Failed to get target PID entry\n");
+    kfree(msg);
+    return -ENOMEM;
+  }
+
+  msg->sender_pid = current->pid;
+  msg->target_pid = target_pid;
+  msg->data_size = msg_size;
+  mutex_lock(&target_entry->lock);
+  list_add(&msg->list, &target_entry->message_list);
+  mutex_unlock(&target_entry->lock);
+  wake_up_interruptible(&target_entry->waitq);
+
   pr_info("minibinder device write\n");
   return count;
 }
